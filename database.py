3b76665d4c76e3523e73253c92bac71742e8be66
Arthur Nicholas <arthurn0606@gmail.com>
2023-06-03 08:24:56 -0300

    criando func para adicionar usuario e configurando variaveis do .env

--- database.py
+++ database.py
@@ -1,26 +1,61 @@
 import mysql.connector
+import datetime
+from zoneinfo import ZoneInfo
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+timezone = ZoneInfo("America/Sao_Paulo")
+
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+database = os.getenv("DB_DATABASE")
+password = os.getenv("DB_PASSWORD")
+port = os.getenv("DB_PORT")
 
 db_config = {
-    'host': '31.220.54.159',
-    'user': 'acessodb',
-    'password': '123456789',
-    'port': '3306'
+    'host': host,
+    'user': user,
+    'database': database,
+    'password': password,
+    'port': port
 }
 
-def get_connection(database):
+def get_connection():
     config = db_config
-    config['database'] = database
     cnx = mysql.connector.connect(**config)
     return cnx
 
-def get_user(database, email):
-    cnx = get_connection(database)
+def create_user(values):
+    global timezone
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    
+    data = datetime.datetime.now(timezone)
+    print(f'Valores: {values}')
+    values = list(values)
+    
+    if len(values) >= 4:  # Verificando se values tem pelo menos 4 elementos
+        query = "INSERT INTO usuarios (nome, email, idade, salario, data_criacao) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(query, (*values, data,))
+    else:
+        query = "INSERT INTO usuarios (nome, email, idade, data_criacao) VALUES (%s, %s, %s, %s)"
+        cursor.execute(query, (*values, data,))
+    
+    print(f'Inserindo dados {values}')
+    
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    
+def get_user(email):
+    cnx = get_connection()
     cursor = cnx.cursor(dictionary=True)
     
     query = "SELECT * FROM usuarios WHERE email = %s"
     cursor.execute(query, (email,))
     
-    result = cursor.fetchall()
+    result = cursor.fetchone()
     
     cursor.close()
     cnx.close()
